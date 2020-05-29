2123d68833dc196cdec37855dade92312c40f1ef
Alex Bormotov <31304073+alex-bormotov@users.noreply.github.com>
2020-05-29 10:07:27 +0300

    v 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# github-telegram
-Bot sends GitHub Trends to Telegram channel
+# Github Trends to Telegram channels
+
+> Bot sends different GitHub Trends to different Telegram channels (three by default) - see config.json.sample and app.py 
+
+> Bot stores only last 30 posts in memory for each channel, when the limit has been reached the bot remove first five items from the end of the each list
+
+![](https://github.com/alex-bormotov/github-telegram/workflows/Github-CICD/badge.svg)   [![Codacy Badge](https://app.codacy.com/project/badge/Grade/c1c1c58f3a1e488091aac12ae1d7127e)](https://www.codacy.com/manual/alex-bormotov/github-telegram?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=alex-bormotov/github-telegram&amp;utm_campaign=Badge_Grade)
+
+## Install (Ubuntu + Docker)
+
+```bash
+git clone https://github.com/alex-bormotov/github-telegram
+```
+
+```bash
+cd github-telegram
+```
+
+```bash
+cp config/config.json.sample config/config.json
+```
+
+> Edit config/config.json
+
+```bash
+sudo chmod +x docker_ubuntu_install.sh && sudo ./docker_ubuntu_install.sh
+```
+
+```bash
+sudo docker run -d --rm --mount src=`pwd`/config,target=/github-telegram/config,type=bind skilfulll1/github-telegram:latest
+```
---
+++ app.py
@@ -0,0 +1,99 @@
+import json
+import requests
+from time import sleep
+from threading import Thread
+
+
+# store to db in future ...
+feed_py = []
+feed_js = []
+feed_trend = []
+
+
+def get_config():
+    with open("config/config.json", "r") as read_file:
+        return json.load(read_file)
+
+
+class Telegram:
+    def __init__(self, chat_id):
+        self.token = get_config()["telegram_token"]
+        self.chat_id = chat_id
+
+    def send_message(self, msg):
+        requests.get(
+            f"https://api.telegram.org/bot{self.token}/sendMessage?text={msg}&chat_id={self.chat_id}")
+
+
+class Feed:
+    def __init__(self, lang, period):
+        self.lang = lang
+        self.period = period
+
+
+    def get_feed(self):
+        url = f'https://ghapi.huchen.dev/repositories?language={self.lang}&since={self.period}'
+        res = requests.get(url).json()
+        return [v for msg in res for k, v in msg.items() if k == 'url']
+
+
+def feed(chat_id, lang, period):
+    global feed_py
+    global feed_js
+    global feed_trend
+
+    try:
+        if lang == 'python':
+            start_feed = feed_py
+        elif lang == 'javascript':
+            start_feed = feed_js
+        elif lang == 'jupyter-notebook':
+            start_feed = feed_trend
+
+        feed_instance = Feed(lang, period)
+        tg = Telegram(chat_id)
+
+        while True:
+            new_feed = feed_instance.get_feed()
+
+            if len(start_feed) == 0:
+                start_feed = new_feed
+
+                for i in start_feed:
+                    tg.send_message(i)
+                    sleep(5)
+
+                if len(start_feed) > 30:
+                    del(start_feed[:5])
+
+            for item in new_feed:
+                if item not in start_feed:
+                    start_feed.append(item)
+                    tg.send_message(item)
+                    sleep(5)
+
+            sleep(7200)
+
+    except Exception as e:
+        tg.send_message(e)
+        print(str(e))
+
+
+def main():
+    channels = [v for k, v in get_config().items() if 'channel' in k]
+
+    t1 = Thread(target=feed, args=(
+        channels[0][0], channels[0][1], channels[0][2]),)
+    t2 = Thread(target=feed, args=(
+        channels[1][0], channels[1][1], channels[1][2]),)
+    t3 = Thread(target=feed, args=(
+        channels[2][0], channels[2][1], channels[2][2]),)
+
+    t1.start()
+    t2.start()
+    t3.start()
+
+
+
+if __name__ == "__main__":
+    main()
